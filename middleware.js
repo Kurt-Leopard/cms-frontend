9ddb37bb034875ae33ff562a5e751f5492ec77e2
jamandre <jamandre.halcyondigital@gmail.com>
2025-01-16 18:02:38 +0800

    chore: route guard

--- middleware.js
+++ middleware.js
@@ -2,15 +2,21 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   console.log("running");
-  const user = "";
+  const user = false;
 
   if (!user) {
     return NextResponse.redirect(new URL("/", request.url));
+  } else {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/" || url.pathname === "") {
+      return NextResponse.rewrite(new URL("/dashboard", request.url));
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/", "/dashboard"],
 };
